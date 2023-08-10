650a54ccaf190275a2c45621fb5c472eb58aba53
CesarRaulG <raulgv.ti20@utsjr.edu.mx>
2023-08-10 01:29:03 -0600

    first commit

---
+++ lib/my_app.dart
@@ -0,0 +1,23 @@
+import 'package:television/screens/extends.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Android TV',
+      initialRoute: '/',
+      routes: {
+        '/': (_) => const MediaProjectionScreen(),
+      },
+    );
+  }
+}
